72b3a109e91e8c13f9e80fa0f992d07360ae81fa
Njabulo505 <nsibanyoni020308@gmail.com>
2026-05-06 12:29:54 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:nj%40bulo_01@localhost:5432/taskdb"
+DATABASE_URL = DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
